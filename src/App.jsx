57ad28025665d00806e525a1dd91a141e64bbdb5
Tathagata Chakraborty <tatu.sonali@gmail.com>
2024-12-03 23:30:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+
+function App() {
+  return (
+    <>
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <Header />
+      <Hero />
+      <Benefits />
+      <Collaboration />
+      <Services />
+    </div>
+    <ButtonGradient />
+
+    </>
+  );
+}
+
+export default App;
