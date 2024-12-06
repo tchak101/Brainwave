eb0c721fbb49cb4ccaa8d28c37e02d248477fe0c
Tathagata Chakraborty <tatu.sonali@gmail.com>
2024-12-07 00:46:27 +0530

    typo fix

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -2,7 +2,7 @@ import { brainwaveSymbol, check } from "../assets";
 import { collabApps, collabContent, collabText } from "../constants";
 import Button from "./Button";
 import Section from "./Section";
-import { LeftCurve, RightCurve } from "./design/collaboration"
+import { LeftCurve, RightCurve } from "./design/Collaboration"
 
 const Collaboration = () => {
   return (
